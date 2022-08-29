3da3e5bd443c3b5b03d0f8d4e724208b99f68472
liyudong <1768756152@qq.com>
2022-08-29 19:03:23 +0800

    feat: log

--- samples/src/main/java/org/uncertaintyman/logtest/Log4jTests.java
+++ samples/src/main/java/org/uncertaintyman/logtest/Log4jTests.java
@@ -2,31 +2,32 @@ package org.uncertaintyman.logtest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
+import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 public class Log4jTests {
 
 
-    static ExecutorService executorService = new ThreadPoolExecutor(
-            10,
-            10,
-            1000,
-            TimeUnit.SECONDS,
-            new ArrayBlockingQueue(1)
-    );
+    static ExecutorService executorService = new ThreadPoolExecutor(10, 10, 1000, TimeUnit.SECONDS, new ArrayBlockingQueue(1));
 
     static Logger logger = LoggerFactory.getLogger(Log4jTests.class);
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        MDC.put("traceId", UUID.randomUUID().toString().replace("-", ""));
 
         logger.info("test......");
 
+        IntStream.range(1, 4).forEach(i -> executorService.execute(() -> logger.info("another thread")));
 
+
+        TimeUnit.SECONDS.sleep(2);
+        executorService.shutdown();
 
     }
 }
