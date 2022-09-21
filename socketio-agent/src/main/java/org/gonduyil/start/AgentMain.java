a77e3769d64076f8204939cb8df35e3934e16769
liyudong <1768756152@qq.com>
2022-09-21 14:11:19 +0800

    feat: modify javassist version

--- socketio-agent/src/main/java/org/gonduyil/start/AgentMain.java
+++ socketio-agent/src/main/java/org/gonduyil/start/AgentMain.java
@@ -6,20 +6,12 @@ public class AgentMain {
 
     public static void premain(String agentOps, Instrumentation inst) {
 
-        System.out.println("premain执行--------------------");
-        //instrument(agentOps, inst);
-        inst.addTransformer(new AuthTransformer());
+        try {
+            System.out.println("premain执行--------------------");
+            inst.addTransformer(new AuthTransformer());
+        } catch (Exception e) {
+
+        }
     }
 
-//    public static void agentmain(String agentOps, Instrumentation inst) {
-//        instrument(agentOps, inst);
-//    }
-
-    /**
-     * agentOps is aop target classname
-     */
-    private static void instrument(String agentOps, Instrumentation inst) {
-        System.out.println(agentOps);
-       // inst.addTransformer(new AOPTransformer(agentOps));
-    }
 }
